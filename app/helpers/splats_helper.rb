81abde284933bb13644fa92872407421eed76148
Jason Zimdars <jz@37signals.com>
2024-08-08 18:06:19 -0500

    Some polish and name change stuff

--- app/helpers/splats_helper.rb
+++ app/helpers/splats_helper.rb
@@ -1,6 +1,6 @@
 module SplatsHelper
   SPLAT_ROTATION = %w[90 80 75 60 45 35 25 5 -45 -40 -75]
-  SPLAT_SIZE = %w[20 18 16 14 12]
+  SPLAT_SIZE = %w[22 18 16 14]
 
   def splat_rotation(splat)
     "--splat-rotate: #{ SPLAT_ROTATION[Zlib.crc32(splat.to_param) % SPLAT_ROTATION.size] }deg;"
