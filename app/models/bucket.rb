8f17152108019caa70795b444a4dbeb18c86e1d8
Jose Farias <jose@37signals.com>
2024-11-07 11:32:44 -0600

    Put includes on the same line

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,6 +1,5 @@
 class Bucket < ApplicationRecord
-  include Filterable
-  include Accessible
+  include Accessible, Filterable
 
   belongs_to :account
   belongs_to :creator, class_name: "User", default: -> { Current.user }
