dc1d9203792f039f4a5a52ec809fa3d719dcef53
Jason Zimdars <jz@37signals.com>
2025-05-02 15:39:10 +0200

    Use black for closed cards only

--- app/models/card/colored.rb
+++ app/models/card/colored.rb
@@ -1,7 +1,7 @@
 module Card::Colored
   extend ActiveSupport::Concern
 
-  COLORS = %w[ #3b3633 #67695e #eb7a32 #bf7c2b #c09c6f #746b1e #2c6da8 #5d618f #663251 #ff63a8 ]
+  COLORS = %w[ #67695e #eb7a32 #bf7c2b #c09c6f #746b1e #2c6da8 #5d618f #663251 #ff63a8 ]
   DEFAULT_COLOR = "#2c6da8"
 
   def color
