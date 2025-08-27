bb04512b0c1d5b7583980214534f92b1b1a5c76b
Andy Smith <andy@37signals.com>
2025-08-27 13:48:47 -0500

    Consolidate header and nav layouts

--- app/controllers/accounts/settings_controller.rb
+++ app/controllers/accounts/settings_controller.rb
@@ -1,4 +1,8 @@
 class Accounts::SettingsController < ApplicationController
+  include FilterScoped
+
+  enable_collection_filtering only: :show
+
   def show
     @account = Account.sole
     @users = User.active
